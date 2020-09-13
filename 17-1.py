63f4e050ede9e3ad358ee63b455f8ec1c7e9d463
rebootedcqwe <2633062020@qq.com>
2020-09-13 15:10:20 +0800

    17

---
+++ 17-1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @version  : Python 3.7
+#定义函数
+def calulus (x):
+    y = x + 1
+    return y
+result = calulus(2)
+print (result)
---
+++ 17-2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @version  : Python 3.7
+#定义函数
+def fruit_function (fruit1, fruit2):
+    fruits = fruit1 + " " + fruit2
+    return fruits
+#调用函数
+result = fruit_function("apple", "banana")
+print (result)
